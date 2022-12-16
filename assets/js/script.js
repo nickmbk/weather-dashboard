c9264d43541c646a7946134dcec525f3408fcba6
Nick <72912456+nickmbk@users.noreply.github.com>
2022-12-16 20:48:11 +0000

    show 5 day forecast with dynamic html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,61 @@
 var API_KEY = "121b7d3d4e8323ac3c4c503bd3a7409f";
+var todaysDate = moment().format("[(]DD[/]MM[/]YYYY[)]")
 
 function search(event) {
     event.preventDefault();
     var location = $("#search-input").val().trim().toLowerCase();
     var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=5&appid=" + API_KEY;
 
-    $.ajax({
-        url: geoURL,
-        method: "GET"
-    }).then(function(response) {
-        var lat = response[0].lat;
-        var lon = response[0].lon;
-        
-        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
-        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + API_KEY;;
-
+    if (location !== "") {
         $.ajax({
-            url: currentQueryURL,
+            url: geoURL,
             method: "GET"
         }).then(function(response) {
-            var locationName = response.name;
-            var todayTemp = Math.floor(response.main.temp - 273.15).toFixed(2);
-            var todayWind = Math.floor(response.wind.speed * 3.6).toFixed(1);
-            var todayHumidity = response.main.humidity;
-            $("#location-date").text(locationName);
-            $("#today-temp").text("Temp: " + todayTemp + " °C");
-            $("#today-wind").text("Wind: " + todayWind + "KPH");
-            $("#today-humidity").text("Humidity: " + todayHumidity + "%");
-        });
-    });
+            var lat = response[0].lat;
+            var lon = response[0].lon;
+            
+            // var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+            var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
 
+            $.ajax({
+                url: forecastQueryURL,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                var fiveDaysArray = [];
+                for (var i = 0; i < response.list.length; i += 8) {
+                    fiveDaysArray.push(response.list[i]);
+                }
+                console.log(fiveDaysArray);
+                var locationName = response.city.name;
+                var todayTemp = (fiveDaysArray[0].main.temp - 273.15).toFixed(2);
+                var todayWind = (fiveDaysArray[0].wind.speed * 3.6).toFixed(1);
+                var todayHumidity = fiveDaysArray[0].main.humidity;
+                var iconCode = fiveDaysArray[0].weather[0].icon;
+                $("#location-date").text(locationName + " " + todaysDate);
+                $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + iconCode + "@2x.png");
+                $("#today-temp").text("Temp: " + todayTemp + " °C");
+                $("#today-wind").text("Wind: " + todayWind + " KPH");
+                $("#today-humidity").text("Humidity: " + todayHumidity + "%");
+
+                for (var i = 0; i < 5; i++) {
+                    var newDiv = $("<div class='forecast'>");
+                    var newDate = $("<p class='date'>");
+                    newDate.text(moment().add((i),'days').format("DD[/]MM[/]YYYY"));
+                    var newIcon = $("<img class='icon' src='http://openweathermap.org/img/wn/" + fiveDaysArray[i].weather[0].icon + "@2x.png'>");
+                    var newTemp = $("<p class='temp'>");
+                    newTemp.text("Temp: " + (fiveDaysArray[i].main.temp - 273.15).toFixed(2) + " °C");
+                    var newWind = $("<p class='wind'>");
+                    newWind.text("Wind: " + (fiveDaysArray[i].wind.speed * 3.6).toFixed(1) + " KPH");
+                    var newHumidity = $("<p class='humidity'>");
+                    newHumidity.text("Humidity: " + fiveDaysArray[i].main.humidity + "%");
+                    newDiv.append(newDate, newIcon, newTemp, newWind, newHumidity);
+                    $("#forecast").append(newDiv);
+                }
+
+            });
+        });
+    }
     
 };
 
