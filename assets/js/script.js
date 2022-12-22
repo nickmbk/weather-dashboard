92fb061ec4476d73aac230d5ee65eed2f6930d09
Nick <72912456+nickmbk@users.noreply.github.com>
2022-12-22 21:46:55 +0000

    insert comments into script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,83 @@
+// variable to store api key for OpenWeatherMap
 var API_KEY = "121b7d3d4e8323ac3c4c503bd3a7409f";
+// use moment.js to get today's date to display on screen
 var todaysDate = moment().format("[(]DD[/]MM[/]YYYY[)]");
+// variables to initially get data from localStorage and turn it into an array to work with later
 var searchHistoryString = localStorage.getItem("locationHistory");
 var searchHistoryItems = JSON.parse(searchHistoryString) ?? [];
 
+// use the searchApi function, using the first entry of the searchHistoryItems array from above as an argument, this is to immediately show the user the last city that was searched for
 searchApi(searchHistoryItems[0]);
 
+// function to create the history buttons
 function createHistoryBtns() {
+  // select the ul with the id of history-list using jQuery, and empty the contents, removing previous dynamically generated HTML
   $("#history-list").empty();
+  // use a for loop to go through the searchHistoryItems array
   for (var i = 0; i < searchHistoryItems.length; i++) {
+    // use a variable to create a button with relevant bootstrap classes, and custom class and a data-location attribute to store the location
     var searchHistoryBtn = $(
       "<button class='history-btn col-12 my-1 border-0 rounded' data-location='" +
         searchHistoryItems[i] +
         "'><li>"
     );
+    // add the location from the array to the text of the newly created button, so the user can see what location the button is refering to
     searchHistoryBtn.text(searchHistoryItems[i]);
+    // append the new button to the history-list ul
     $("#history-list").append(searchHistoryBtn);
   }
 }
 
+// function to save the history of locations searched to local storage for persistant data
 function saveHistory(locationName) {
+  // check to see if the location entered by the user is already in the searchHistoryItems array, and if it exists, what index it lives at is stored in checkLocation variable
   var checkLocation = searchHistoryItems.indexOf(locationName);
+  // if the location doesn't exist in the history then we can carry on past this if statement, but if it does exist...
   if (checkLocation !== -1) {
+    // ... remove it from the array
     searchHistoryItems.splice(checkLocation, 1);
   }
+  // add the location to the beginning of the array
   searchHistoryItems.unshift(locationName);
+  // if the array holds more than 10 entries, remove the last one, I only want to keep a history of 10 so it doesn't get too messy
   if (searchHistoryItems.length > 10) {
     searchHistoryItems.pop();
   }
+  // stringify the array
   searchHistoryString = JSON.stringify(searchHistoryItems);
+  // save to localStorage
   localStorage.setItem("locationHistory", searchHistoryString);
+  // pull it back out of localStorage to keep our working array updated
   searchHistoryString = localStorage.getItem("locationHistory");
   searchHistoryItems = JSON.parse(searchHistoryString) ?? [];
+  // go to the createHistoryBtns function to create the buttons
   createHistoryBtns();
 }
 
+// function that will search the API's and create the dynamic html to display the weather to the user, taking a parameter of location
 function searchApi(location) {
+  // variable to store and use the geo API to find the long and lat coordinates for the location entered by the user
   var geoURL =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     location +
     "&limit=5&appid=" +
     API_KEY;
 
+  // if the location text box isn't empty (the user may have clicked search without typing anything in), then proceed
   if (location !== "") {
+    // use ajax within jQuery to get data from the Geo API for the long and lat coordinates 
     $.ajax({
+      // using the geoURL variable above as the address
       url: geoURL,
       method: "GET",
+    // when the data is returned
     }).then(function (response) {
+      // store the lat data from the response in the lat variable
       var lat = response[0].lat;
+      // and the lon data from the response in the llon variable
       var lon = response[0].lon;
 
+      // now we create the url for the weather API using the lon and lat variable to tell the API the location
       var forecastQueryURL =
         "http://api.openweathermap.org/data/2.5/forecast?lat=" +
         lat +
@@ -56,12 +85,19 @@ function searchApi(location) {
         lon +
         "&appid=" +
         API_KEY;
-
+      
+        // use ajax again to get the data from the weather API
       $.ajax({
         url: forecastQueryURL,
         method: "GET",
+      // once we get a response from the API then do this...
       }).then(function (response) {
+        // empty the sections with the ids today and forecast
+        $("#today").empty();
+        $("#forecast").empty();
+        // create a new array to store the details of the next five days in 24 hour blocks
         var fiveDaysArray = [];
+        // use a for loop to go through the array of data in the API response add add that data to our own fiveDaysArray array, we are going up in eights in the for loop, because each 24 hour period is stored in every 8th array index
         for (var i = 0; i < response.list.length; i += 8) {
           fiveDaysArray.push(response.list[i]);
         }
@@ -133,8 +169,6 @@ function searchApi(location) {
 
 function search(event) {
   event.preventDefault();
-  $("#today").empty();
-  $("#forecast").empty();
   if ($(event.target).attr("data-location") === undefined) {
     var location = $("#search-input").val().trim().toLowerCase();
   } else {
